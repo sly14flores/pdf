81a5905d004386aa520ef32fe3da456d8d531c49
Sly Flores <sly@christian.com.ph>
2016-09-03 00:42:03 +0800

    Init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('fpdf/fpdf.php');
+require_once('fpdi/fpdi.php');
+
+// initiate FPDI
+$pdf = new FPDI();
+// add a page
+$pdf->AddPage();
+// set the source file
+$pdf->setSourceFile("pds2005.pdf");
+// import page 1
+$tplIdx = $pdf->importPage(1);
+// use the imported page and place it at position 0, 0 with original width
+$pdf->useTemplate($tplIdx, null, null, 0, 0, true);
+
+// now write some text above the imported page
+$pdf->SetFont('Arial', '', 8);
+$pdf->SetTextColor(0, 0, 0);
+$pdf->SetXY(150, 34);
+$pdf->Write(0, 'This is just a simple text');
+
+$pdf->Output();
+
+?>
