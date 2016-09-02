21f5257f6ec5aaba44bd21f1a41729f19aecedd8
Sly Flores <sly@christian.com.ph>
2016-09-03 01:01:27 +0800

    Grid

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ $pdf->AddPage();
 // set the source file
 $pdf->setSourceFile("pds2005.pdf");
 // import page 1
-$tplIdx = $pdf->importPage(1);
+$tplIdx = $pdf->importPage(2);
 // use the imported page and place it at position 0, 0 with original width
 $pdf->useTemplate($tplIdx, null, null, 0, 0, true);
 
 // now write some text above the imported page
 $pdf->SetFont('Arial', '', 8);
 $pdf->SetTextColor(0, 0, 0);
-$pdf->SetXY(150, 34);
+$pdf->SetXY(150, 1);
 $pdf->Write(0, 'This is just a simple text');
 
 $pdf->Output();
